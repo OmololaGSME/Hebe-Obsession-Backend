be35292b4a3238bee91ba171a1e82213fe4b40b8
Slimobedsama <ohwo.okoro@yahoo.com>
2024-07-17 03:00:04 +0100

    set-up of email transport function

---
+++ src/utils/mail.ts
@@ -0,0 +1,28 @@
+import { createTransport } from 'nodemailer';
+
+const emailSender = async(from: string, to: string, subject: string, html: string)=> {
+    try {
+        const transport = createTransport({
+            host: process.env.EMAIL_HOST,
+            port: Number(process.env.EMAIL_PORT),
+            auth: {
+              user: process.env.EMAIL_USER,
+              pass: process.env.PASS
+            }
+          });
+          const mailOptions = {
+            from: from,
+            to: to,
+            subject: subject,
+            html: html
+          }
+          const info = await transport.sendMail(mailOptions);
+          console.log(`Email sent ${ info.messageId }`);
+    } catch (err) {
+        if(err instanceof Error) {
+            console.log(err.message)
+        }
+    }
+}
+
+export default emailSender;
