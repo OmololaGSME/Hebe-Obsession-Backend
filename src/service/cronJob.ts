43aea7bd4bf221173da03b0e2ed724f7009375a3
Slimobedsama <ohwo.okoro@yahoo.com>
2024-10-02 02:49:14 +0100

    removed logging of fetched data

--- src/service/cronJob.ts
+++ src/service/cronJob.ts
@@ -6,7 +6,6 @@ const job = ()=> {
         console.log('Server staying awake');
         const response = await fetch('https://hebe-beauty.onrender.com/health-check');
         const data = await response.json()
-        console.log(data);
     })
 }
 
